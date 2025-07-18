931dea5d6a1d54ef7375262028425deb784a6c2d
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-07-18 18:19:50 +0300

    Updated the login and signup form

--- task-manager/src/pages/Login.jsx
+++ task-manager/src/pages/Login.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import { login } from '../api';
 import { useNavigate, Link } from 'react-router-dom';
+import "../App.css";
 
 function Login() {
   const [form, setForm] = useState({ email: '', password: '' });
   const navigate = useNavigate();
 const handleSubmit = async (e) => {
   e.preventDefault();
-  console.log('Form submitted:', form); // ✅
+  console.log('Form submitted:', form); 
 
   try {
     const { data } = await login(form);
-    console.log('Login success:', data); // ✅
+    console.log('Login success:', data); 
     localStorage.setItem('token', data.token);
     localStorage.setItem('user', JSON.stringify(data.user)); 
-    console.log('Navigating to /dashboard...'); // ✅
+    console.log('Navigating to /dashboard...'); 
     navigate('/dashboard');
     console.log('After navigate'); // Should never appear
   } catch (err) {
@@ -25,37 +26,42 @@ const handleSubmit = async (e) => {
 
 
   return (
- <div className="min-h-screen flex items-center justify-center">
-  <form onSubmit={handleSubmit} className="card w-96 bg-base-100 shadow-xl p-8">
-    <fieldset className="bg-base-200 border border-base-300 rounded-box p-1 w-full max-w-sm mx-auto">
+ <div className="min-h-screen flex   justify-center text-left items-center pb-3">
+  <form onSubmit={handleSubmit} className="card w-1/4 bg-base-100 shadow-xl p-8 ">
+    <fieldset className="bg-base-200 border border-base-300 rounded-box w-full max-w-sm mx-auto">
       <legend className="fieldset-legend text-bold">Login</legend>
 
-
+     
+     <br />
       <div className="form-control mb-3">
         <label htmlFor="email" className="label">
-          <span className="label-text">Email</span>
+          Email
         </label>
+        <br />
         <input
           id="email"
           type="email"
-          className="input input-bordered "
+          className="input input-bordered form-input input-sm"
           placeholder="Email"
           onChange={e => setForm({ ...form, email: e.target.value })}
         />
       </div>
-
-   
+    <br />
+     
         <label htmlFor="password" className="label">
-          <span className="label-text">Password</span>
+         Password
         </label>
+        <br />
         <input
           id="password"
           type="password"
-          className="input input-bordered"
+          className="input input-bordered form-input input-sm"
           placeholder="Password"
           onChange={e => setForm({ ...form, password: e.target.value })}
         />
-     
+    
+     <br />
+     <br />
 
       <div className="mt-6">
         <button type="submit" className="btn btn-neutral w-full">Login</button>
--- task-manager/src/pages/Register.jsx
+++ task-manager/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { register } from '../api';
 import { useNavigate, Link } from 'react-router-dom';
+import "../App.css";
 
 function Register() {
   const [form, setForm] = useState({  firstName:'', lastName:'',email: '', password: '' });
@@ -18,62 +19,76 @@ function Register() {
   };
 
   return (
- <div className="min-h-screen flex items-center justify-center">
-  <form onSubmit={handleSubmit} className="card w-96 bg-base-100 shadow-xl p-6">
-    <fieldset className="bg-base-200 border border-base-300 rounded-box p-4 w-full max-w-sm mx-auto">
+ <div className="min-h-screen flex justify-center text-left items-center pb-3">
+  <form onSubmit={handleSubmit} className="card w-1/4 bg-base-100 shadow-xl p-8 ">
+    <fieldset className="bg-base-200 border border-base-300 rounded-box p-4 w-96  max-w-sm mx-auto">
       <legend className="text-lg font-bold">Register</legend>
 
+     
+     <br />
       <div className="form-control mb-3">
         <label htmlFor="firstName" className="label">
-          <span className="label-text">First Name</span>
+         First Name
         </label>
+        <br />
+       
         <input
           id="firstName"
           type="text"
-          className="input input-bordered"
+          className="input input-bordered input-sm form-input"
           placeholder="First Name"
           onChange={e => setForm({ ...form, firstName: e.target.value })}
         />
       </div>
 
+     <br />
+
       <div className="form-control mb-3">
         <label htmlFor="lastName" className="label">
-          <span className="label-text">Last Name</span>
+          Last Name
         </label>
+         <br />
         <input
           id="lastName"
           type="text"
-          className="input input-bordered"
+          className="input input-bordered input-sm form-input"
           placeholder="Last Name"
           onChange={e => setForm({ ...form, lastName: e.target.value })}
         />
       </div>
 
+     <br />
+
       <div className="form-control mb-3">
         <label htmlFor="email" className="label">
-          <span className="label-text">Email</span>
+         Email
         </label>
+         <br />
         <input
           id="email"
           type="email"
-          className="input input-bordered"
+          className="input input-bordered input-sm form-input"
           placeholder="Email"
           onChange={e => setForm({ ...form, email: e.target.value })}
         />
       </div>
+     <br />
+
 
       <div className="form-control mb-4">
         <label htmlFor="password" className="label">
-          <span className="label-text">Password</span>
+         Password
         </label>
+         <br />
         <input
           id="password"
           type="password"
-          className="input input-bordered"
+          className="input input-bordered input-sm form-input"
           placeholder="Password"
           onChange={e => setForm({ ...form, password: e.target.value })}
         />
       </div>
+       <br />
 
       <div className="mt-6">
         <button type="submit" className="btn btn-neutral w-full">Register</button>
