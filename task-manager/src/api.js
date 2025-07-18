9de260850a30b122f53dc198d119aabf850623df
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-07-18 15:13:33 +0300

    Updated the api.js

--- task-manager/src/api.js
+++ task-manager/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 API.interceptors.request.use((req) => {
