6baf489b3388ce2bedb920791afa8751ae0a2dd3
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-07-18 14:11:41 +0300

    Updated the mongodb connection

--- server/server.js
+++ server/server.js
@@ -15,10 +15,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI,
-   { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
+mongoose.connect(process.env.MONGO_URI,{})
   .then(() => app.listen(5000, () => console.log('Server started on port 5000')))
   .catch(err => console.log(err));
